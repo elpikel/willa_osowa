a15cf14cae799ba5bbc921d92d1476fb219eb066
Arkadiusz Plichta <el.pikel@gmail.com>
2026-09-22 21:14:07 +0200

    fix

--- lib/willa_osowa_web/controllers/page_controller.ex
+++ lib/willa_osowa_web/controllers/page_controller.ex
@@ -1,12 +1,16 @@
 defmodule WillaOsowaWeb.PageController do
   use WillaOsowaWeb, :controller
 
-  @index Application.app_dir(:willa_osowa, "priv/static/index.html")
-
   def index(conn, _params) do
     conn
     |> put_resp_content_type("text/html")
     |> put_resp_header("cache-control", "no-cache")
-    |> send_file(200, @index)
+    |> send_file(200, index_path())
+  end
+
+  # Resolved at runtime so it points at the release's priv dir, not the
+  # compile-time build path.
+  defp index_path do
+    Path.join(:code.priv_dir(:willa_osowa), "static/index.html")
   end
 end
